d2b897bde69c01c450df46e9e2feb7ccd20d88c8
aalmiray <aalmiray@gmail.com>
2019-05-07 13:20:46 +0200

    Update build dependencies

--- src/main/java/javax/application/event/tck/EventBusTest.java
+++ src/main/java/javax/application/event/tck/EventBusTest.java
@@ -20,9 +20,15 @@ package javax.application.event.tck;
 import org.junit.jupiter.api.Test;
 
 import javax.application.event.EventBus;
+import javax.application.event.EventFilter;
 import javax.application.event.EventHandler;
+import javax.application.event.EventMetadata;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -78,6 +84,51 @@ public abstract class EventBusTest {
         assertTrue(eventHandler.event instanceof Event1);
     }
 
+    @Test
+    public void publish_event_with_filters() {
+        // given:
+        EventBus eventBus = resolveEventBus();
+        TestEvent3Handler eventHandler = new TestEvent3Handler();
+        eventBus.subscribe(eventHandler);
+
+        //when:
+        eventBus.publishEvent(new Event1());
+        eventBus.publishEvent(new Event2());
+        eventBus.publishEvent(new Event3(false));
+
+        // then:
+        assertEquals(0, eventHandler.called);
+        assertNull(eventHandler.event);
+
+        //when:
+        eventBus.publishEvent(new Event1());
+        eventBus.publishEvent(new Event2());
+        eventBus.publishEvent(new Event3(true));
+
+        // then:
+        assertEquals(1, eventHandler.called);
+        assertTrue(eventHandler.event instanceof Event3);
+    }
+
+    @Test
+    public void publish_event_honors_priority() {
+        // given:
+        EventBus eventBus = resolveEventBus();
+        TestEventHandlerPrio1 eventHandler1 = new TestEventHandlerPrio1();
+        TestEventHandlerPrio2 eventHandler2 = new TestEventHandlerPrio2();
+        eventBus.subscribe(eventHandler1);
+        eventBus.subscribe(eventHandler2);
+        Event4 event = new Event4();
+
+        //when:
+        eventBus.publishEvent(event);
+
+        // then:
+        assertEquals(2, event.handlers.size());
+        assertSame(eventHandler1, event.handlers.get(0));
+        assertSame(eventHandler2, event.handlers.get(1));
+    }
+
     public static abstract class AbstractTestEventHandler {
         protected Event event;
         protected int called;
@@ -113,6 +164,39 @@ public abstract class EventBusTest {
         }
     }
 
+    public static class TestEvent3Handler extends AbstractTestEventHandler {
+        @EventHandler(filters = {Event3Filter.class})
+        public void handleEvent3(Event3 event) {
+            this.event = event;
+            this.called++;
+        }
+    }
+
+    public static class Event3Filter implements EventFilter<Event3> {
+        @Override
+        public Boolean apply(EventMetadata<Event3> metadata) {
+            return metadata.getEvent().isActive();
+        }
+    }
+
+    public static class TestEventHandlerPrio1 extends AbstractTestEventHandler {
+        @EventHandler(priority = 1)
+        public void handleEvent4(Event4 event) {
+            this.event = event;
+            this.called++;
+            event.handlers.add(this);
+        }
+    }
+
+    public static class TestEventHandlerPrio2 extends AbstractTestEventHandler {
+        @EventHandler(priority = 2)
+        public void handleEvent4(Event4 event) {
+            this.event = event;
+            this.called++;
+            event.handlers.add(this);
+        }
+    }
+
     public interface Event {
 
     }
@@ -121,5 +205,25 @@ public abstract class EventBusTest {
     }
 
     public static class Event2 implements Event {
+    }
+
+    public static class Event3 implements Event {
+        private boolean active;
+
+        public Event3(boolean active) {
+            this.active = active;
+        }
+
+        public boolean isActive() {
+            return active;
+        }
+
+        public void setActive(boolean active) {
+            this.active = active;
+        }
+    }
+
+    public static class Event4 implements Event {
+        private final List<Object> handlers = new ArrayList<>();
     }
 }
